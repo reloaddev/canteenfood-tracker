ed0f3e00fec00af7d411ef28f07925fef4aa8334
derReloader <62664722+derReloader@users.noreply.github.com>
2022-12-03 21:07:33 +0100

    Add typescript support

--- website/canteenfood-tracker/vite.config.js
+++ website/canteenfood-tracker/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { sveltePreprocess } from "svelte-preprocess/dist/autoProcess";
 
 export default defineConfig({
   resolve: {
@@ -7,5 +8,5 @@ export default defineConfig({
       "~bootstrap": "../node_modules/bootstrap",
     },
   },
-  plugins: [svelte()],
+  plugins: [svelte({ preprocess: sveltePreprocess() })],
 });
